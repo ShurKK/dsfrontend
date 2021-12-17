6bd10d38812f569f9f2000c191fdc2cc55493b76
Alexander Revtov <alexander.revtov@gmail.com>
2021-12-17 21:53:11 +0200

    highlight selected episode feture

--- src/components/episode_card.js
+++ src/components/episode_card.js
@@ -7,11 +7,16 @@ import Img from "gatsby-image"
 
 const EpisodeCard = ({ episode }) => {
   const { appState, updateAppState } = useContext(ContextContainer);
+  let episodeCardClass = "episode-card";
+  if (appState.picked_episode == episode.node.number) { // highlight selected episode
+    episodeCardClass = "episode-card selected"
+    }; 
+
   return (
    // <Link to={`/episode/${episode.node.slug}`} className="uk-link-reset"> // <Img  loading="eager" fluid={episode.node.thumb.localFile.childImageSharp.fluid} />
    <>  
    <a href="/" onClick={(e) => {e.preventDefault();updateAppState({ ...appState, picked_episode: episode.node.number }); } }>
-    <div className="episode-card-1">
+      <div className={episodeCardClass}> {/* highlight selected episode */} 
         <GatsbyImage
            
             alt={`Picture for ${episode.node.name} episode`}
